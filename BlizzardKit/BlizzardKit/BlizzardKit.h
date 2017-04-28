059633e5dd8a12e30d451204400835d01d441075
Stephan Leroux <sleroux@mozilla.com>
2017-04-28 17:22:57 -0400

    Initial commit

---
+++ BlizzardKit/BlizzardKit/BlizzardKit.h
@@ -0,0 +1,19 @@
+//
+//  BlizzardKit.h
+//  BlizzardKit
+//
+//  Created by Steph Leroux on 2017-04-28.
+//  Copyright © 2017 Stephan Leroux. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BlizzardKit.
+FOUNDATION_EXPORT double BlizzardKitVersionNumber;
+
+//! Project version string for BlizzardKit.
+FOUNDATION_EXPORT const unsigned char BlizzardKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlizzardKit/PublicHeader.h>
+
+
